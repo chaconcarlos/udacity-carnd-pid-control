4993dc4032aa72e62cf1cd66fcb3447af9ac4d85
Carlos Chacon <carlos.rivas@smartmatic.com>
2017-07-10 11:09:48 -0500

    Some adjustments to hyperparameters in P4

--- src/PidController.cpp
+++ src/PidController.cpp
@@ -12,9 +12,9 @@
 //static const double INITIAL_P_VALUE    = 0.165; /* Initial Parameter for P */
 //static const double INITIAL_I_VALUE    = 0.000; /* Initial Parameter for I */
 //static const double INITIAL_D_VALUE    = 3;     /* Initial Parameter for D */
-static const double INITIAL_P_VALUE    = 0.23595;
-static const double INITIAL_I_VALUE    = 0.0030;
-static const double INITIAL_D_VALUE    = 2.757;
+static const double INITIAL_P_VALUE    = 0.259545;
+static const double INITIAL_I_VALUE    = 0.0050;
+static const double INITIAL_D_VALUE    = 5.757;
 static const int    SETTLE_STEPS_COUNT = 100;
 static const int    TWIDDLE_LOOP_STEPS = 1150;
 static const int    TUNE_STEPS_TOTAL   = SETTLE_STEPS_COUNT + TWIDDLE_LOOP_STEPS;
--- src/main.cpp
+++ src/main.cpp
@@ -34,7 +34,7 @@ int main()
 
   PidController pid;
 
-  pid.enableParameterTuning();
+  //pid.enableParameterTuning();
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
